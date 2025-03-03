93a95866e3df4f3f51692d3dcdca198e15060b74
Neel Shah <neelasheshbhai.shah@sjsu.edu>
2025-03-02 18:50:57 -0800

    Add navigation bar

---
+++ Romato-Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+import NavBar from "./components/ui/NavBar"
+function App() {
+  return (
+    <div className="w-screen h-screen bg-neutral-950 text-white flex items-center justify-center">
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
