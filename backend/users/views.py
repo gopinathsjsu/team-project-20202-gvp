cd4daa84f2198a467cae946eb7bca5a6bce4c165
Aarav Shah <aaravpshah2409@gmail.com>
2025-03-31 18:27:10 -0700

    registration and login

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,8 +4,10 @@ from rest_framework import status
 from .serializers import UserSerializer
 from .models import User
 from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import AllowAny
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny] 
     def post(self, request):
         data = request.data
         data['password_hash'] = make_password(data['password'])
@@ -14,3 +16,21 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny] 
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid username or password'}, status=status.HTTP_404_NOT_FOUND)
+        
+        if user.check_password(password):
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid username or password'}, status=status.HTTP_400_BAD_REQUEST)
